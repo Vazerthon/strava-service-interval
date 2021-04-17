0995b282d68382fb7ac597efdf6abdc3d484d3a7
Paul Vallis <vallis1@gmail.com>
2021-04-17 12:13:27 +0100

    Add button to test code exchange

--- src/containers/Auth.js
+++ src/containers/Auth.js
@@ -1,16 +1,27 @@
 import { useContext } from 'react';
 import qs from 'qs';
+import axios from 'axios';
 
 import { SettingsContext } from '../contexts/Settings';
 
 export default function Auth() {
-  const { stravaAuthUrl } = useContext(SettingsContext);
-  const { code, error } = qs.parse(window.location.search, { ignoreQueryPrefix: true });
+  const { stravaAuthUrl, makePublicTokenExchangeUrl } = useContext(SettingsContext);
+  const { code, error } = qs.parse(window.location.search, {
+    ignoreQueryPrefix: true,
+  });
 
-  return <>
-    { error && <>There's an error</> }
-    { code && <>Your code is {code}</> }
-    
-    <a href={stravaAuthUrl}>Connect with Strava</a>
-  </>;
+  const exchangeCode = (accessCode) => () =>
+    axios
+      .get(makePublicTokenExchangeUrl(accessCode))
+      .then(console.log);
+
+  return (
+    <>
+      {error && <>There's an error</>}
+      {code && <>Your code is {code}</>}
+
+      <a href={stravaAuthUrl}>Connect with Strava</a>
+      <button onClick={exchangeCode(code)}>exchange code</button>
+    </>
+  );
 }
--- src/settings.js
+++ src/settings.js
@@ -3,11 +3,13 @@ const stravaRedirectUrl = process.env.REACT_APP_STRAVA_REDIRECT_URL;
 const stravaAuthUrl = `https://www.strava.com/oauth/authorize?client_id=${stravaClientId}&response_type=code&redirect_uri=${stravaRedirectUrl}&approval_prompt=force&scope=read`;
 const makeStravaTokenExchangeUrl = (stravaClientSecret, accessCode) =>
   `https://www.strava.com/api/v3/oauth/token?grant_type=authorization_code&client_id=${stravaClientId}&client_secret=${stravaClientSecret}&code=${accessCode}`;
+const makePublicTokenExchangeUrl = (accessCode) => `.netlify/functions/stravaTokenExchange?code=${accessCode}`;
 
 const settings = {
   stravaClientId,
   stravaAuthUrl,
   makeStravaTokenExchangeUrl,
+  makePublicTokenExchangeUrl,
 };
 
 export default settings;

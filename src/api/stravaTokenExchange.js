1c026ffe8bdef5d66e6b6d1bb1b3e2e9f7775339
Paul Vallis <vallis1@gmail.com>
2021-04-17 11:39:25 +0100

    Transpile lambdas to commonjs

--- src/api/stravaTokenExchange.js
+++ src/api/stravaTokenExchange.js
@@ -1,7 +1,7 @@
-const axios = require('axios');
-const { settings } = require('../contexts/Settings');
+import axios from 'axios';
+import { settings } from '../contexts/Settings';
 
-exports.handler = async function handler(event, context) {
+export async function handler(event, context) {
   const { STRAVA_SECRET_KEY } = process.env;
   const url = settings.makeStravaTokenExchangeUrl('x', 'y');
 

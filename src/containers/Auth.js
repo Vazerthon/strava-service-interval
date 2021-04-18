8480aee5625856d5dc97efb150f771d4409e3408
Paul Vallis <vallis1@gmail.com>
2021-04-18 18:45:59 +0100

    Connect and pull athlete data

--- src/containers/Auth.js
+++ src/containers/Auth.js
@@ -3,25 +3,34 @@ import qs from 'qs';
 import axios from 'axios';
 
 import { SettingsContext } from '../contexts/Settings';
+import { useState } from 'react';
 
 export default function Auth() {
-  const { stravaAuthUrl, makePublicTokenExchangeUrl } = useContext(SettingsContext);
-  const { code, error } = qs.parse(window.location.search, {
-    ignoreQueryPrefix: true,
-  });
+  const { stravaAuthUrl, makePublicTokenExchangeUrl, stravaAthleteUrl } = useContext(SettingsContext);
+  const { code, error } = qs.parse(window.location.search, { ignoreQueryPrefix: true });
+  const [stravaData, setStravaData] = useState(undefined);
 
-  const exchangeCode = () => {
-    const url = makePublicTokenExchangeUrl(code);
-    axios.get(url).then(({ data }) => console.log(data)).catch(console.log);
+  if (code) {
+    axios
+      .get(makePublicTokenExchangeUrl(code))
+      .then(({ data }) => setStravaData(data))
+      .catch(console.log);
+  }
+
+  if (stravaData) {
+    axios
+      .get(stravaAthleteUrl, {
+        headers: { Authorization: `Bearer: ${stravaData.access_token}` }
+      })
+      .then(({ data }) => console.log(data))
+      .catch(console.log);
   }
 
   return (
     <>
       {error && <div>There's an error</div>}
-      {code && <div>Your code is {code}</div>}
 
       <a href={stravaAuthUrl}>Connect with Strava</a>
-      <button onClick={exchangeCode}>exchange code</button>
     </>
   );
 }
--- src/settings.js
+++ src/settings.js
@@ -3,12 +3,14 @@ const stravaRedirectUrl = process.env.REACT_APP_STRAVA_REDIRECT_URL;
 const stravaAuthUrl = `https://www.strava.com/oauth/authorize?client_id=${stravaClientId}&response_type=code&redirect_uri=${stravaRedirectUrl}&approval_prompt=force&scope=read`;
 const stravaTokenExchangeUrl = 'https://www.strava.com/api/v3/oauth/token';
 const makePublicTokenExchangeUrl = (accessCode) => `.netlify/functions/stravaTokenExchange?code=${accessCode}`;
+const stravaAthleteUrl = 'https://www.strava.com/api/v3/athlete';
 
 const settings = {
   stravaClientId,
   stravaAuthUrl,
   stravaTokenExchangeUrl,
   makePublicTokenExchangeUrl,
+  stravaAthleteUrl,
 };
 
 export default settings;

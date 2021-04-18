29d550aa571642bdf3cb8b5b528a62c2fd2198a4
Paul Vallis <vallis1@gmail.com>
2021-04-18 17:10:46 +0100

    Extract token exchange function

--- src/api/stravaTokenExchange.js
+++ src/api/stravaTokenExchange.js
@@ -1,31 +1,46 @@
 import axios from 'axios';
+import qs from 'qs';
 import settings from '../settings';
+
+export const makeTokenExchangeRequest = (
+  stravaClientSecret,
+  stravaClientId,
+  stravaTokenExchangeUrl,
+  code,
+) => {
+  const data = {
+    code,
+    client_id: stravaClientId,
+    client_secret: stravaClientSecret,
+    grant_type: 'authorization_code',
+  };
+
+  const options = {
+    method: 'POST',
+    responseType: 'json',
+    headers: { 'content-type': 'application/x-www-form-urlencoded' },
+    data: qs.stringify(data),
+    url: stravaTokenExchangeUrl,
+  };
+
+  const onSuccess = (result) => ({
+    statusCode: 200,
+    body: JSON.stringify(result),
+  });
+  const onError = () => ({ statusCode: 400 });
+
+  return axios(options).then(onSuccess).catch(onError);
+};
 
 export function handler({ queryStringParameters }) {
   const { STRAVA_SECRET_KEY, STRAVA_CLIENT_ID } = process.env;
   const { code } = queryStringParameters;
   const { stravaTokenExchangeUrl } = settings;
 
-  const data = {
+  return makeTokenExchangeRequest(
+    STRAVA_SECRET_KEY,
+    STRAVA_CLIENT_ID,
+    stravaTokenExchangeUrl,
     code,
-    client_id: STRAVA_CLIENT_ID,
-    client_secret: STRAVA_SECRET_KEY,
-    grant_type: 'authorization_code',
-  };
-
-  const options = {
-    method: 'POST',
-    url: stravaTokenExchangeUrl,
-    data,
-  };
-
-  return axios(options)
-    .then((result) => ({
-      statusCode: 200,
-      body: JSON.stringify(result),
-    }))
-    .catch(() => ({
-      statusCode: 400,
-      body: 'error',
-    }));
+  );
 }
--- src/containers/Auth.js
+++ src/containers/Auth.js
@@ -1,27 +1,27 @@
 import { useContext } from 'react';
 import qs from 'qs';
-import axios from 'axios';
+import { makeTokenExchangeRequest } from '../api/stravaTokenExchange';
 
 import { SettingsContext } from '../contexts/Settings';
 
 export default function Auth() {
-  const { stravaAuthUrl, makePublicTokenExchangeUrl } = useContext(SettingsContext);
+  const { stravaAuthUrl, stravaTokenExchangeUrl } = useContext(SettingsContext);
   const { code, error } = qs.parse(window.location.search, {
     ignoreQueryPrefix: true,
   });
 
-  const exchangeCode = (accessCode) => () =>
-    axios
-      .get(makePublicTokenExchangeUrl(accessCode))
-      .then(console.log);
+  const exchangeCode = () =>
+    makeTokenExchangeRequest('', '', stravaTokenExchangeUrl, code).then(
+      console.log,
+    );
 
   return (
     <>
-      {error && <>There's an error</>}
-      {code && <>Your code is {code}</>}
+      {error && <div>There's an error</div>}
+      {code && <div>Your code is {code}</div>}
 
       <a href={stravaAuthUrl}>Connect with Strava</a>
-      <button onClick={exchangeCode(code)}>exchange code</button>
+      <button onClick={exchangeCode}>exchange code</button>
     </>
   );
 }

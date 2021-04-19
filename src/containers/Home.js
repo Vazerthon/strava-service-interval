f8cecc693d958eb26ef4def2a682816f9c869bdf
Paul Vallis <vallis1@gmail.com>
2021-04-19 19:03:19 +0100

    Get athlete data on home page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,25 @@
-import { useContext } from 'react';
+import axios from 'axios';
+import { useContext, useEffect } from 'react';
 import { useHistory } from 'react-router';
 import { SettingsContext } from '../contexts/Settings';
 import { StravaContext } from '../contexts/Strava';
 
 export default function Home() {
   const history = useHistory();
-  const { routes } = useContext(SettingsContext);
+  const { routes, stravaAthleteUrl } = useContext(SettingsContext);
   const { stravaData } = useContext(StravaContext);
+
+  useEffect(() => {
+    if (!stravaData) {
+      return;
+    }
+
+    axios
+      .get(stravaAthleteUrl, {
+        headers: { Authorization: `Bearer ${stravaData.accessToken}` },
+      })
+      .then(({ data }) => console.log(data));
+  }, [stravaAthleteUrl, stravaData]);
 
   if (!stravaData) {
     history.push(routes.welcome);

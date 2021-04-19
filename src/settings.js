289a3def9023ff2683a926f59a592bdc7ab12850
Paul Vallis <vallis1@gmail.com>
2021-04-19 19:26:02 +0100

    Update scope

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 const stravaClientId = process.env.REACT_APP_STRAVA_CLIENT_ID;
 const stravaRedirectUrl = process.env.REACT_APP_STRAVA_REDIRECT_URL;
-const stravaAuthUrl = `https://www.strava.com/oauth/authorize?client_id=${stravaClientId}&response_type=code&redirect_uri=${stravaRedirectUrl}&approval_prompt=force&scope=read_all`;
+const stravaAuthUrl = `https://www.strava.com/oauth/authorize?client_id=${stravaClientId}&response_type=code&redirect_uri=${stravaRedirectUrl}&approval_prompt=force&scope=profile:read_all`;
 const stravaTokenExchangeUrl = 'https://www.strava.com/api/v3/oauth/token';
 const makePublicTokenExchangeUrl = (accessCode) => `.netlify/functions/stravaTokenExchange?code=${accessCode}`;
 const stravaAthleteUrl = 'https://www.strava.com/api/v3/athlete';

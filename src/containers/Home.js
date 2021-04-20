25f9eb45e906c152aac79fb1430f691538d5fb41
Paul Vallis <vallis1@gmail.com>
2021-04-20 09:23:30 +0100

    Delete commented code

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,25 +1,12 @@
-import axios from 'axios';
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import { useHistory } from 'react-router';
 import { SettingsContext } from '../contexts/Settings';
 import { StravaContext } from '../contexts/Strava';
 
 export default function Home() {
   const history = useHistory();
-  const { routes, stravaAthleteUrl } = useContext(SettingsContext);
+  const { routes } = useContext(SettingsContext);
   const { stravaData } = useContext(StravaContext);
-
-  // useEffect(() => {
-  //   if (!stravaData) {
-  //     return;
-  //   }
-
-  //   axios
-  //     .get(stravaAthleteUrl, {
-  //       headers: { Authorization: `Bearer ${stravaData.accessToken}` },
-  //     })
-  //     .then(({ data }) => console.log(data));
-  // }, [stravaAthleteUrl, stravaData]);
 
   if (!stravaData) {
     history.push(routes.welcome);
